bab44b15c2fbf695cb281c254b46954a72f17c9e
alpharalph <ralph.slabihoud@reminds.at>
2023-06-13 21:28:56 +0200

    adapt app program to call distinct async server start

--- RemoteMvpApp/Program.cs
+++ RemoteMvpApp/Program.cs
@@ -5,13 +5,15 @@ using RemoteMvpClient;
 using RemoteMvpLib;
 
 
+// Bootstrapper sequence
+
 // Server-side classes
-var server = new RemoteActionEndpoint("localhost",11000);
-var app = new ApplicationController(server);
-            
+var endpoint = new RemoteActionEndpoint("localhost",11000);
+var server = new ApplicationController(endpoint);
+//server.RunActionEndPoint();                       // Synchronous (blocking) server start
+Task serverTask = server.RunActionEndPointAsync();  // Asynchronous (non-blocking) server start
+
 // Client-side classes
 var client = new RemoteActionAdapter("localhost", 11000);
-var clientController = new ClientController(client);
+var clientController = new ClientPresenter(client);
 clientController.OpenUI(true);
-
-
