3b373c0efa1d09b7fc2150bf17578d191be7da20
alpharalph <ralph.slabihoud@reminds.at>
2023-06-13 21:22:57 +0200

    add message classes

---
+++ RemoteMvpServer/RemoteActionMessages.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace RemoteMvpLib
+{
+    public enum ActionType
+    {
+        Register,
+        Login,
+        Logout
+    }
+
+    public class RemoteActionRequest
+    {
+        public ActionType Type { get; }
+
+        public string UserName { get; }
+
+        public string Password { get; }
+
+        public RemoteActionRequest(ActionType type, string username, string password)
+        {
+            Type = type;
+            UserName = username;
+            Password = password;
+        }
+    }
+
+    public enum ResponseType
+    {
+        Success,
+        Error
+    }
+
+    public class RemoteActionResponse
+    {
+        public ResponseType Type { get; set; }
+
+        public string? Message { get; set; }
+
+        public RemoteActionResponse(ResponseType type, string? message)
+        {
+            Type = type;
+            Message = message;
+        }
+    }
+}
