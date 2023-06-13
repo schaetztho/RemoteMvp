48939c25f3fc45b90f8debc35e34dfd6c900bbf0
alpharalph <ralph.slabihoud@reminds.at>
2023-06-13 21:28:10 +0200

    change client program to actually use mvp the presenter

--- RemoteMvp/Program.cs
+++ RemoteMvp/Program.cs
@@ -1,3 +1,5 @@
+using RemoteMvpLib;
+
 namespace RemoteMvpClient
 {
     internal static class Program
@@ -11,7 +13,9 @@ namespace RemoteMvpClient
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ClientView());
+            var client = new RemoteActionAdapter("localhost", 11000);
+            var clientController = new ClientPresenter(client);
+            clientController.OpenUI(true);
         }
     }
 }
